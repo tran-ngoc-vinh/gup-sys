4622413c9ebc8169a3482e7c392fab1433753f29
tran-ngoc-vinh <tran-ngoc-vinh@morien.com>
2021-02-09 17:47:10 +0900

    int

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,29 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'gup_sys1' );
+// define( 'DB_NAME', 'gup_sys1' );
+
+// /** MySQL database username */
+// define( 'DB_USER', 'gup_sys1' );
+
+// /** MySQL database password */
+// define( 'DB_PASSWORD', '123456789' );
+
+// /** MySQL hostname */
+// define( 'DB_HOST', 'gup-sys1-web.c8bwsaqlxbq6.ap-northeast-1.rds.amazonaws.com' );
+
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'gup-sys' );
 
 /** MySQL database username */
-define( 'DB_USER', 'gup_sys1' );
+define( 'DB_USER', 'gup-sys' );
 
 /** MySQL database password */
 define( 'DB_PASSWORD', '123456789' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'gup-sys1-web.c8bwsaqlxbq6.ap-northeast-1.rds.amazonaws.com' );
+define( 'DB_HOST', 'localhost' );
+
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
