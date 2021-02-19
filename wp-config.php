c29627a707219105f302504223a19e9b41555bcb
tran-ngoc-vinh <tran-ngoc-vinh@morien.com>
2021-02-20 08:47:47 +0900

    int

--- wp-config.php
+++ wp-config.php
@@ -32,16 +32,16 @@
 // define( 'DB_HOST', 'localhost' );
 
 // /** The name of the database for WordPress */
-define( 'DB_NAME', 'web_gup_sys' );
+define( 'DB_NAME', 'gup_sys_com' );
 
 /** MySQL database username */
-define( 'DB_USER', 'web_gup_sys' );
+define( 'DB_USER', 'gup_sys_com' );
 
 /** MySQL database password */
 define( 'DB_PASSWORD', '123456789' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'web-gup-sys-web.c8bwsaqlxbq6.ap-northeast-1.rds.amazonaws.com' );
+define( 'DB_HOST', 'gup-sys-com-web.c8bwsaqlxbq6.ap-northeast-1.rds.amazonaws.com' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
